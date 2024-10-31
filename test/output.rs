9016de1b81b2c13e6b584c263dbbf3c5c638b4d2
MrChaker <mr.chakerw@gmail.com>
2024-10-31 11:22:57 +0100

    Update sample

--- test/output.rs
+++ test/output.rs
@@ -1,27 +1,17 @@
 view! {
-    <vxmlSample attr1="mom" attr2="dad">
-        r#"this is a text childwith two lines"#
-        <html>
-            r#"this is a text childwith two lines"#<header charset="utf-8"></header>
-            <body>
-                r#"this is a text childwith two lines"#<header charset="utf-8"></header>
-                <div>
-                    r#"this is a text childwith two lines"#<header charset="utf-8"></header>
-                    r#"some textmore textfurther text"#r#"consecutive textnode"#
-                </div>
-            </body>
-            <goda>
-                r#"this is a text childwith two lines"#<header charset="utf-8"></header>
-                <body>
-                    r#"this is a text childwith two lines"#<header charset="utf-8"></header>
-                    <div>
-                        r#"this is a text childwith two lines"#<header charset="utf-8"></header>
-                        r#"some textmore textfurther text"#r#"consecutive textnode"#
-                    </div>
-                </body><child1 attr5="value23"></child1><child2></child2>
-                r#"hello I am child3, but the first text child"#<child4></child4>
-            </goda>
-        </html>
-    </vxmlSample>
-    <yello></yello>
+    <Section>
+        r#"lorem lorem lorem lorem lorem"#<Image src="images"></Image>r#"math $Math$ Follow up"#
+        <Exercises>
+            r#"lorem lorem lorem lorem lorem"#<Image src="images"></Image>r#"math $Math$ Follow up"#
+            <Exercise>
+                r#"lorem lorem lorem lorem lorem"#<Image src="images"></Image>
+                r#"math $Math$ Follow up"#r#"Exercise Content"#
+                <Grid cols=3>
+                    r#"lorem lorem lorem lorem lorem"#<Image src="images"></Image>
+                    r#"math $Math$ Follow up"#r#"Exercise Content"#<Item></Item><Item></Item>
+                    <Item></Item>
+                </Grid>
+            </Exercise>
+        </Exercises>
+    </Section>
 }
